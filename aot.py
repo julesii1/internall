6c57077c10bf9c1afbda265441ee25d3aac155e8
James Brown <84690155+jamoms@users.noreply.github.com>
2022-04-06 22:13:48 +0000

    Added pop gui for perimeter of triangle

---
+++ aot.py
@@ -0,0 +1,42 @@
+from tkinter import *
+import tkinter as trik
+
+def calculate_area():
+	base = float(entry1.get())
+	height = float(entry2.get())
+	area = 1 / 2 * base * height 
+	output_label.configure(text = ' Area of Triangle= {:.2f} ' .format(area))
+
+def back():
+  exec(open('main.py').read())
+  exit()
+
+def exit():
+   triangle_window.destroy()
+  
+triangle_window = trik.Tk()
+triangle_window.geometry("700x400")
+triangle_window.config(bg="#c7d6f5")
+triangle_window.resizable(width=False,height=False)
+triangle_window.title('Python GUI TUTORIALS')
+
+message_label1 = trik.Label(text= 'Enter base: ' ,font=( ' Verdana ' , 18))
+message_label2 = trik.Label(text= ' Enter height: ' ,font=( ' Verdana ' , 18))
+output_label = trik.Label(font=( ' Verdana ' , 18), text='Area of Triangle is:')
+entry1 = trik.Entry(font=( ' Verdana ' , 18), width=6)
+entry2 = trik.Entry(font=( ' Verdana ' , 18), width=6)
+calc_button = trik.Button(text= ' Calculate Area ' , font=( ' Verdana ' , 16),command=calculate_area)
+back_button = trik.Button(triangle_window,text="Go Back!",font=("Arial",13),command=back)
+
+
+message_label1.grid(row=1, column=0)
+entry1.grid(row=1, column=1)
+
+message_label2.grid(row=2, column=0)
+entry2.grid(row=2, column=1)
+
+calc_button.grid(row=1, column=2, rowspan=2)
+back_button.grid(row=1, column=4, rowspan=5)
+output_label.grid(row=3, column=0, columnspan=3)
+mainloop()
+
--- main.py
+++ main.py
@@ -14,15 +14,12 @@ from tkinter import ttk
 
 def pop():
   window.destroy()
-  exec(open('gui1.py').read())
+  exec(open('aot.py').read())
   
-def find_ap (shape, aside, bside, cside):
+def find_ap (shape):
 
     # gets the five entries
   s = shape_chosen.current()
-  a = e_aside.get()
-  b = e_bside.get()
-  c = e_cside.get()
   f = form_chosen.current()
   
   # all formulas for area
@@ -41,43 +38,16 @@ def display_calc_ap(ap):
 def validation():
   # gets the five entries
   s = shape_chosen.current()
-  a = e_aside.get()
-  b = e_bside.get()
-  c = e_cside.get()
   f = form_chosen.current()
-  
-  msg = ''
-  if len(a) == 0 or len (b) == 0 or len (c) == 0:
-      msg = 'aside, shape and bside can\'t be empty'
-  else:
-    try:
-      if any(ch.isdigit() for ch in a) == False:
-        msg = 'aside must be a NUMBER'
-      elif s == 0:
-        msg = 'choose an appropriate SHAPE'
-      elif any(ch.isdigit() for ch in b) == False:
-        msg = 'bside must be a NUMBER'
-      elif any(ch.isdigit() for ch in c) == False:
-        msg = 'cside must be a NUMBER'
-      else:
-       # msg = 'Success!'
-        shape = s #shape is already in number from list position
-        aside = int(a)
-        bside = int(b)
-        cside = int(c)
 
-        if f == 'Area':
-          print("Area Chosen")
-        elif f == 'Perimeter':
-          print("Perimeter Chosen")      
-        calc_ap = find_ap(shape, aside, bside, cside)
-        display_calc_ap(calc_ap)
-            
-    except Exception as ep:
-      messagebox.showerror('error', ep)
+  if f == 'Area':
+   print("Area Chosen")
+  elif f == 'Perimeter':
+    print("Perimeter Chosen")      
+  calc_ap = find_ap("shape")
+  display_calc_ap(calc_ap)
+          
       
-  if msg != '':
-    messagebox.showinfo('message', msg)
 def exit():
     window.destroy()
 # ____________   MAIN  ________________
@@ -90,23 +60,21 @@ window.config(bg="#c7d6f5")
 window.resizable(width=False,height=False)
 window.title('Area Perimeter Calculator')
 
-
+root = Tk()      
+canvas = Canvas(root, width = 300, height = 300)      
+canvas.pack()      
+img = PhotoImage(file="ball.ppm")      
+canvas.create_image(20,20, anchor=NW, image=img)      
+mainloop() 
 
 # Labels for Heading and Subheadng of GUI
 lb_heading = tk.Label(window,text="Area Perimeter Calculator",font=("Arial", 20),fg="black",bg="#c7d6f5")
 lb_subheading = tk.Label(window,font=("Arial",12),text="Input shape and numbers",fg="black",bg="#c7d6f5")
  
-# Labels for yrside, shape and bside
+# Labels for shape, ap
 lb_shape = tk.Label(window,text = "Shape: ",font=('Arial',12,"bold"),fg="#000000",bg="#c7d6f5")
 lb_ap = tk.Label(window,text = "Area Or Perimeter:",font=
 ('Arial',12,"bold"),fg="#000000",bg="#c7d6f5")
-lb_aside = tk.Label(window,text = "A Side:",font=('Arial',12,"bold"),fg="#000000",bg="#c7d6f5")
-lb_bside = tk.Label(window,text = "B Side:",font=('Arial',12,"bold"),fg="#000000",bg="#c7d6f5")
-lb_cside = tk.Label(window,text = "C Side:",font=('Arial',12,"bold"),fg="#000000",bg="#c7d6f5")
-# Entry boxes for shape,aside and bside and cside
-e_aside = tk.Entry(window,width=5)
-e_bside = tk.Entry(window,width=5)
-e_cside = tk.Entry(window,width=5)
 
 # Combobox creation
 n = tk.StringVar()
@@ -158,21 +126,12 @@ btn_exit = tk.Button(window,text="Exit Application!",font=("Arial",13),command=e
 # Placing the elements on the screen
 lb_heading.place(x=180,y=5)
 lb_subheading.place(x=20,y=50)
-lb_ap.place(x=20,y=230)
+lb_ap.place(x=20,y=150)
 #month
 lb_shape.place(x=20,y=110)
-#date
-lb_aside.place(x=20,y=140)
-e_aside.place(x=120,y=140)
-#year
-lb_bside.place(x=20,y=170)
-e_bside.place(x=120,y=170)
-#cside
-lb_cside.place (x=20, y=200)
-e_cside.place (x=120, y=200)
 #e_shape.place(x=120,y=105)
 shape_chosen.place(x=120,y=110)
-form_chosen.place(x=205,y=230)
+form_chosen.place(x=205,y=150)
 btn_calculate_ap.place(x=30,y=350)
 lb_calculated_ap.place(x=310,y=70)
 tbox_ap.place(x=425,y=100)
